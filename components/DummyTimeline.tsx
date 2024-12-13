9ea05be819ccd279ccd70eedcbb80f4f1b3a44b9
suyash5641 <suyashjai05@gmail.com>
2024-12-14 01:49:32 +0530

    fix

--- components/DummyTimeline.tsx
+++ components/DummyTimeline.tsx
@@ -4,7 +4,7 @@ import { Seeker } from "./Seeker";
 const DummyTimeline = () => {
   const array = new Array(4).fill(0);
   return (
-    <div className="flex flex-row bg-background fixed bottom-[40px] h-[110px]  md:relative md:bottom-auto w-full  md:h-[150px] bg-gradient-to-b from-transparent to-gray-900/10 backdrop-blur-sm border-t border-gray-200/10">
+    <div className="flex flex-row items-center bg-background fixed bottom-[40px] h-[110px]  md:relative md:bottom-auto w-full  md:h-[150px] bg-gradient-to-b from-transparent to-gray-900/10 backdrop-blur-sm border-t border-gray-200/10">
       <Seeker height="h-[80%]" left="left-10" />
       <div className="flex gap-1 transition-transform duration-200 ml-[3.2rem] rounded-md border-2 border-white bg-[#242121]">
         {array.map((frame, index) => (
